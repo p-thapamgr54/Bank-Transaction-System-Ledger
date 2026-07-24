fe24c08a8c889aeb3769ba738ce0cd46be9f3a69
Pankaj Thapa <p.thapamgr54@gmail.com>
2026-07-24 21:51:04 +0545

    Adding Middleware To Parse Incoming JSON Requests

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import authRoutes from "./routes/auth.routes.js"; // Importing the auth routes
 
 const app = express(); // Creating an instance of the Express application
-
+app.use(express.json()); // Middleware to parse incoming JSON requests
 app.use("/api/v2/auth", authRoutes); // Mounting the auth routes on the /api/v2/auth path
 
 export default app;
